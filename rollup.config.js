b0f7f32f6e405b6211d017083b1c4640c99e64d9
Sachin Singh <ssingh259@sapient.com>
2019-02-23 19:29:44 +0530

    Created initial project

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import babel from "rollup-plugin-babel";
+import { uglify } from "rollup-plugin-uglify";
+
+export default [
+    {
+        input: "src/js/demo/demo.js",
+        output: {
+            file: "dist/demo/demo.js",
+            format: "iife",
+            sourcemap: true
+        },
+        plugins: [
+            postcss({
+                extensions: ['.css'],
+                minimize: true,
+                sourceMap: true,
+                extract: true
+            }),
+            resolve({
+                customResolveOptions: {
+                    moduleDirectory: "node_modules"
+                }
+            }),
+            commonjs(),
+            babel({
+                exclude: "node_modules/**"
+            }),
+            uglify()
+        ]
+    }
+]
