1e8ebc868746869336b464ff7214f7acacfa438c
Sachin Singh <ssingh259@sapient.com>
2021-11-10 21:42:19 +0530

    Updated demo

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,33 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import postcss from "rollup-plugin-postcss";
-import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default [
-    {
-        input: "src/js/demo/demo.js",
-        output: {
-            file: "dist/demo/demo.js",
-            format: "iife",
-            sourcemap: true
+  {
+    input: 'src/js/demo/demo.js',
+    output: {
+      file: 'dist/demo/demo.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    plugins: [
+      postcss({
+        extensions: ['.css'],
+        minimize: true,
+        sourceMap: true,
+        extract: true,
+      }),
+      resolve({
+        mainFields: ['main'],
+        customResolveOptions: {
+          moduleDirectories: ['node_modules'],
         },
-        plugins: [
-            postcss({
-                extensions: ['.css'],
-                minimize: true,
-                sourceMap: true,
-                extract: true
-            }),
-            resolve({
-                mainFields: ["main"],
-                customResolveOptions: {
-                    moduleDirectory: "node_modules"
-                }
-            }),
-            commonjs({
-                namedExports: {
-                    "silkrouter": ["router", "route", "unroute", "routeIgnoreCase", "param", "deparam", "routeParams"]
-                }
-            }),
-            babel(),
-            terser()
-        ]
-    }
-]
+      }),
+      commonjs(),
+      babel(),
+      terser(),
+    ],
+  },
+];
